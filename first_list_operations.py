833f5579749372571b18e30a368b58633a2f1f47
harshul970 <harshul_j970@yahoo.com>
2019-12-11 00:22:00 +0530

    Create first_list_operations.py

---
+++ first_list_operations.py
@@ -0,0 +1,29 @@
+l1 = [
+    2,5,7,8,11,18,23,31
+]
+
+## Initializing lists
+list_odds = []
+list_evens = []
+list_sqr = []
+
+## Getting all odd nos. in this list
+for i in l1:
+    if i%2!=0:                  # Checking whether the number is odd
+        list_odds.append(i)     # Adding to the odd nos list
+
+print(list_odds)
+
+## Getting all even nos. in this list
+for i in l1:
+    if i%2==0:                  # Checking whether the number is even
+        list_evens.append(i)     # Adding to the even nos list
+
+print(list_evens)
+
+list_reverse = l1[::-1]
+print(list_reverse)
+
+## Calculating square of each list number
+list_sqr = [ i*i for i in l1]
+print(list_sqr)
